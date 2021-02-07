36acf0865fbb5aea49130d32a48c9d76a59ddcc0
moondory <moondory777@gmail.com>
2021-02-08 01:30:41 +0900

    base commit

---
+++ Assets/Editor/CustomPopup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+class CustomPopup : EditorWindow
+{
+    static CustomPopup customPopup;
+
+    [MenuItem("Window/CustomPopup")]
+    static void Open()
+    {
+        if (customPopup == null) {
+            customPopup = CreateInstance<CustomPopup>();
+        }
+        customPopup.ShowPopup();
+    }
+
+    private void OnGUI()
+    {
+        if (Event.current.keyCode == KeyCode.Escape)
+        {
+            customPopup.Close();
+        }
+    }
+}
+
---
+++ Assets/Editor/CustomPopupWindow.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CustomPopupWindow : EditorWindow {
+
+    [MenuItem("Window/CustomPopupWindow")]
+    static void Open() {
+        GetWindow<CustomPopupWindow>();
+    }
+
+    CustomPopupContent popupContent = new CustomPopupContent();
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("CustomPopupContent", GUILayout.Width(256)))
+        {
+            var activatorRect = GUILayoutUtility.GetLastRect();
+
+            PopupWindow.Show(activatorRect, popupContent);
+        }
+    }
+}
+
+public class CustomPopupContent : PopupWindowContent
+{
+    public override void OnGUI(Rect rect)
+    {
+        EditorGUILayout.LabelField("Label");
+    }
+
+    public override void OnOpen()
+    {
+        base.OnOpen();
+        Debug.Log("OnOpen()");
+    }
+
+    public override void OnClose()
+    {
+        base.OnClose();
+        Debug.Log("OnClose()");
+    }
+
+    public override Vector2 GetWindowSize()
+    {
+        return base.GetWindowSize();
+    }
+}
+
